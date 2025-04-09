fa842c647a11ddf07dbbe738b4ba358543f5222d
Suryasahitsimhadri <simhadrisuryasahit@gmail.com>
2025-04-09 08:04:20 +0530

    Update 03_LSTM_Model.py

--- (Deployment) WEB-App/pages/03_LSTM_Model.py
+++ (Deployment) WEB-App/pages/03_LSTM_Model.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import os
 import io
+import pickle
 
 from model_handlers.lstm_handler import LSTMModelHandler
 from utils import set_page_config, save_uploaded_file
@@ -25,10 +26,10 @@ lstm_handler = LSTMModelHandler()
 st.sidebar.header("Model Settings")
 
 # Model selection
-model_path = st.sidebar.text_input("Model Path", placeholder="Enter path to your LSTM model file (.h5)")
+model_path = st.sidebar.text_input("Model Path", placeholder="Enter path to your LSTM model file")
 
 # Option to upload a model file
-uploaded_model = st.sidebar.file_uploader("Or upload a model file", type=['h5', 'keras'])
+uploaded_model = st.sidebar.file_uploader("Or upload a model file", type=['h5', 'keras', 'pkl])
 if uploaded_model is not None:
     # Save the uploaded model to a temporary file
     model_path = save_uploaded_file(uploaded_model)
